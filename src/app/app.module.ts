c504a52145f788e822d195b30b8776c399b1cbe8
eslawski <eslawski@umich.edu>
2017-03-08 17:40:28 -0500

    Create angular 2 project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ColorSquareComponent } from './color-square/color-square.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ColorSquareComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/color-square/color-square.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'color-square',
+  templateUrl: './color-square.component.html',
+  styleUrls: ['./color-square.component.css']
+})
+export class ColorSquareComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
